43cb2fc405a9832f8294397357a999192ef1f3aa
SAKotov <mozz.kotov@gmail.com>
2021-08-05 07:14:55 +0300

    working with files

--- working_with_files.php
+++ working_with_files.php
@@ -1,3 +1,83 @@
 <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 
+//$arr = ['1.txt', '2.txt', '3.txt'];
+//$newFile = '';
+//
+//foreach ($arr as $key)
+//{
+//    file_put_contents($key, 'hello');
+//    $newFile .= file_get_contents($key);
+//}
+//file_put_contents('new.txt', $newFile);
+
+
+//$file = file_get_contents('test.txt');
+//
+//file_put_contents('test.txt', $file * $file);
+
+
+//$file = file_get_contents('count.txt');
+//file_put_contents('count.txt', $file + 1);
+
+
+//$arr = ['1.txt', '2.txt', '3.txt'];
+//$newFile = '';
+//
+//foreach ($arr as $key)
+//{
+//    $file = file_get_contents($key);
+//    file_put_contents($key, $file. '!');
+//}
+
+
+//rename('next.txt', 'folder/next.txt');
+
+
+//var_dump(file_exists('count11.txt'));
+
+
+//if (file_exists('count.txt'))
+//{
+//    unlink('count.txt');
+//}   else
+//    file_put_contents('count.txt', 'hey');
+
+
+//$arr = ['1.txt', '2.txt', '3.txt'];
+//
+//foreach ($arr as $item) {
+//    if (file_exists($item))
+//    {
+//        echo 'Существует';
+//    }
+//    else
+//    {
+//        echo 'Такого файла нет';
+//    }
+//}
+
+
+//$arr = explode(' ', file_get_contents('test.txt'));
+//
+//var_dump($arr);
+
+//mkdir('folder1');
+
+
+//$arr = ['hi', 'hello', 'privet'];
+//
+//foreach ($arr as $item) {
+//    mkdir($item);
+//}
+
+//$arr1 = ['1.txt', '2.txt', '3.txt'];
+//
+//foreach ($arr1 as $item) {
+//    file_put_contents($item, 'hey');
+//    rename($item,"folder/$item");
+//}
+
+//rename('folder', 'folder2');
 
