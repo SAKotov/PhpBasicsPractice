a0eecd5e5055373d8112268c261a1fdbc5398e1a
SAKotov <mozz.kotov@gmail.com>
2021-08-05 22:04:14 +0300

    add working_with_sessions.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 error_reporting(E_ALL);
+ini_set('display_errors', 'on');
 include 'working_with_php.php';
 include 'work_with_GET_requests.php';
 include 'regular_expressions.php';
 include 'working_with_files.php';
+include 'working_with_sessions.php';
