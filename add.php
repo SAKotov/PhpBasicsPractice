ea5621cf0a02d05498251e4f05edf9409c3bba6a
SAKotov <mozz.kotov@gmail.com>
2021-08-20 13:38:36 +0300

    add pagination.php

--- add.php
+++ add.php
@@ -1,15 +1,29 @@
 <?php
+
+
 $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
 $user = 'root'; //имя пользователя, по умолчанию это root
 $password = 'root'; //пароль, по умолчанию пустой
 $db_name = 'test'; //имя базы данных
 
-//Соединяемся с базой данных используя наши доступы:
-
-
 
 
 $link = mysqli_connect($host, $user, $password, $db_name);
+
+function input($name)
+{
+    if (isset($_POST[$name]))
+    {
+        $value = $_POST[$name];
+    } else
+    {
+        $value = '';
+    }
+
+
+    return '<input name="' . $name . '" value="' . $value .'">';
+}
+
 if (!empty($_POST))
 {
     $name = $_POST['name'];
@@ -20,10 +34,10 @@ if (!empty($_POST))
     mysqli_query($link, $query) or die (mysqli_error($link));
 }
 ?>
-
+`
 <form action="" method="POST">
-    Имя <input name="name" value="<?php if (isset($_POST['name'])) echo $_POST['name']; ?>">
-    Возраст <input type="text" name="age" value="<?php if (isset($_POST['age'])) echo $_POST['age']; ?>">
-    ЗП <input type="text" name="salary" value="<?php if (isset($_POST['salary'])) echo $_POST['salary']; ?>">
-    Добавить работника <input type="submit">
+    Имя<?php echo input('name');?>
+    Возраст<?php echo input('age');?>
+    ЗП<?php echo input('salary');?>
+     <input type="submit" value="добавить работника">
 </form>
