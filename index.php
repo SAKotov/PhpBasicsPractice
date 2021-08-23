f900f33ce20c9779ac02dbc4b85111dbb1ee26a4
SAKotov <mozz.kotov@gmail.com>
2021-08-23 20:52:19 +0300

    test

--- index.php
+++ index.php
@@ -9,4 +9,41 @@ ini_set('display_errors', 'on');
 //include 'working_with_sessions.php';
 //include 'add.php';
 //include 'working_with_DB.php';
-include 'pagination.php';
+//include 'pagination.php';
+
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div id="wrapper">
+        <header>
+            <?php include 'engine_creation/elements/header.php'?>
+        </header>
+        <main>
+            <?php
+                $page = $_GET['page'];
+                $path = "engine_creation/pages/$page.php";
+
+                if (file_exists($path))
+                {
+                    include $path;
+                } else
+                {
+                    echo 'page not found';
+                }
+            ?>
+        </main>
+        <footer>
+            <?php include 'engine_creation/elements/footer.php'?>
+        </footer>
+    </div>
+</body>
+</html>
